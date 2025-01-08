24c6e6ce9317ba808f2cf8add085ddc8314aae10
AhmedAbdoHammad <143012114+AhmedAbdoHammad@users.noreply.github.com>
2025-01-08 15:05:49 +0200

    Update INTERFACE.h

--- Main Code/TrafficLights/INTERFACE.h
+++ Main Code/TrafficLights/INTERFACE.h
@@ -16,6 +16,7 @@
 
 #ifndef INTERFACE.h
 #define INTERFACE.h
+#include "STD_TYPES.h"
 /***************************************************************************************************/
 /* Function Name  : DisplayNumbers
    Function Type  : Displaying
